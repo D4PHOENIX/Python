ed26e5580969ee8e728df59b6ba244227c8088a8
Daud Noman <daudnoman0@gmail.com>
2023-09-30 17:26:32 +0500

    suiiiiiii

---
+++ Practice/discount.py
@@ -0,0 +1,14 @@
+Username="Farhan"
+print("My name is", Username)
+valBeforeDiscount = 200 
+# NM= 200
+percentDiscount = 20
+# A="20%"
+taxRate = 10
+# B="10%"
+print("Total Cart Value before Discount =", valBeforeDiscount)
+print("Discount Percentage =", percentDiscount)
+print("Tax Rate =", taxRate)
+discountAmount = valBeforeDiscount * percentDiscount / 100
+# d="NM*A/100"
+print("The Discount Amount is ", discountAmount)
